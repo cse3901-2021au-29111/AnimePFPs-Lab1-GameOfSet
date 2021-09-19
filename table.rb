75b2d573edf3c2b1df44466f909121c20cdb9d59
Cameron <camjan0624@gmail.com>
2021-09-19 18:10:25 -0400

    Added comments outlining how to tackle 2 methods, and edited main to prevent runtime errors

--- table.rb
+++ table.rb
@@ -39,21 +39,34 @@ class Table
     @table_size
   end
 
+  #Prints out each card that's on the table to the console
   def display_table
+    loc = 0
     for card in @cards_showing
+      print("#{loc}")
       card.present_card()
-      puts("")
+      loc += 1
+      print("\033[39;49m")
     end
   end
 
+  #Compares 3 cards, removes them if they are a set, or outputs a message telling the user they were not a valid set
+  # assumes that there are no duplicate numbers in nums
+  # (Needs done), nums is an array of 3 numbers corresponding to the location of a card in cards_showing
+  # d is the deck to draw from
   def remove_set(nums)
     0
   end
 
+  #Checks if there are any matches of cards on the table
+  # (Needs Done), basically check every possible combination of cards on the table
+  # if there are zero matches then return 0, else return 1(should try and use a break to kill the loop if one is found)
   def has_matches
     1
   end
 
+  #Shuffles the current table into the deck, then redraws to table_max cards
+  # (Might need fixed, check after every other method is implemented)
   def shuffle(d)
     d.add_to_deck(@cards_showing)
     @table_max.times {@cards_showing.push(d.draw_card)}
