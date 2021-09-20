c06efe234c9317446c99916a2dac6b6ea0df5fbb
gr0004 <gautamrao277@gmail.com>
2021-09-20 10:52:21 -0400

    Added implementations or atleast a skeleton to has_matchess and remove_set in table.rb. We can edit them if the implementations are incorrect.

--- table.rb
+++ table.rb
@@ -47,11 +47,26 @@ class Table
   end
 
   def remove_set(nums)
-    0
+    #remove all (only 1 if cards are unique) the occurrences of nums in @cards_showing
+    @cards_showing.difference(nums)
   end
 
   def has_matches
-    1
+    #iterate over cards_showing to determine if there exist any sets on the table.
+    # continue statement skips the loop if it is iterating through the same element in the outer loop.
+    for c1 in @cards_showing
+      for c2 in @cards_showing
+        continue if c1 == c2
+        for c3 in @cards_showing
+          continue if c1 == c3 || c2 == c3
+          is_set = c1.check_match(c2) == c2.check_match(c3) && c2.check_match(c3 )== c1.check_match(c3)
+          #return 1 if is_set is true.
+          return 1 if is_set
+        end
+      end
+    end
+    #return 0 if no set is found.
+    0
   end
 
   def shuffle(d)
